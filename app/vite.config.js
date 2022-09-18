fe69f2b3b64e112d36663e6cc6fc170c30515c7d
Расуль Арсланов <rasularslanoov@gmail.com>
2022-09-18 19:57:31 +0500

    Removed unused code

--- app/vite.config.js
+++ app/vite.config.js
@@ -22,9 +22,4 @@ export default defineConfig({
       },
     },
   },
-  /*define: {
-    __VUE_I18N_FULL_INSTALL__: true,
-    __VUE_I18N_LEGACY_API__: false,
-    __INTLIFY_PROD_DEVTOOLS__: false,
-  },*/
 })
